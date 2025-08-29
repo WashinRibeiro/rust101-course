e7f334527336116f7b0626bda32c1aec240f5c20
Washington Ribeiro <washinuchiha@gmail.com>
2025-08-29 15:04:04 -0300

    feat(variables): correcting name errors

--- variaveis/src/nomes.rs
+++ variaveis/src/nomes.rs
@@ -22,51 +22,51 @@
 
 #[test]
 fn test_variavel_de_letra_unica() {
-    let a = "Valor";
+    let valor = "Valor";
     // Este teste falha porque estamos tentando desencorajar o uso de nomes de variáveis de uma letra.
-    assert!(false, "Utilize um nome mais descritivo em vez de `a`.");
+    assert!(true, "Utilize um nome mais descritivo em vez de `a`.");
 }
 
 #[test]
 fn test_variavel_com_numero() {
-    let var123 = "Número da Conta";
+    let number_account = "Número da Conta";
     // Este teste falha porque números no nome da variável devem ter um significado claro.
     assert!(
-        false,
+        true,
         "Utilizar um nome descritivo sem números ou com números significativos."
     );
 }
 
 #[test]
 fn test_variavel_com_acento() {
-    let variável = "portugues";
+    let language = "portugues";
     // Este teste falha porque acentos no nome da variável podem causar "erros".
-    assert!(false, "Utilizar um nome descritivo em ingles.");
+    assert!(true, "Utilizar um nome descritivo em ingles.");
 }
 
 #[test]
 fn test_variavel_caso_misto() {
-    let Xx = "Algum valor";
+    let xx = "Algum valor";
     // Este teste falha porque estamos tentando desencorajar o uso de mistura de maiúsculas e minúsculas sem um padrão claro.
-    assert!(false, "Use snake_case para nomes de variáveis em Rust.");
+    assert!(true, "Use snake_case para nomes de variáveis em Rust.");
 }
 
 #[test]
 fn test_variavel_caso_de_camelo() {
-    let helloWorld = "Olá Mundo";
+    let hello_world = "Olá Mundo";
     // Este teste falha porque não estamos seguindo a convenção snake_case.
     assert!(
-        false,
+        true,
         "Os nomes das variáveis devem estar em snake_case e não em camelCase."
     );
 }
 
 #[test]
 fn test_variavel_nao_descritiva() {
-    let minha_casa = 122000;
+    let cnpj = 122000;
     // Este teste falha porque o nome da variável não é descritivo.
     assert!(
-        false,
+        true,
         "Escolha um nome para a variável que descreva o seu objetivo ou conteúdo."
     );
 }
@@ -75,9 +75,7 @@ fn test_variavel_nao_descritiva() {
 fn test_sem_variavel_numero_magico() {
     // Este teste falha porque um valor sem variável torna o código confuso
     // Neste caso, o número 4.90 deveria ser uma variável `dolar_price`.
-
-    assert!(
-        4.90 * 100.00 == 490.00,
-        "Escolha um nome para a variável para o 4.90"
-    );
+    let dolar_price: f64 = 4.90;
+    assert!(format!("{:.2}", dolar_price * 100.00) == "490.00",
+    "Escolha um nome para a variável para o 4.90");
 }
